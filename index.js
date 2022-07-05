ba0c0e79c6cf8a734129dae5277ae8f78982ab43
Vladimir <55217434+vshkliaryk@users.noreply.github.com>
2022-07-05 14:44:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/post.routes')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+
+const corsOptions = {
+  origin: "*"
+}
+
+app.use(cors(corsOptions))
+
 app.use(express.json())
 
 app.use('/api', userRouter)
